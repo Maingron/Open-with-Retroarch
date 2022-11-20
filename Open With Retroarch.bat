f383339699851f1e9a9aafd7901d3a1900980da7
Maingron <r@maingron.com>
2022-11-20 17:23:58 +0100

    Set version to 0.2

--- Open With Retroarch.bat
+++ Open With Retroarch.bat
@@ -1,5 +1,6 @@
 @echo off
 @REM Author: Maingron
+@REM Version: 0.2
 @REM Description: Open a game with Retroarch
 @REM Website: https://maingron.com
 @REM Dependencies: Retroarch (https://retroarch.com)
