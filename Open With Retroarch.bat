89d7339c11ae9ec0346cd83d3724c84e8a2b5a40
Maingron <r@maingron.com>
2022-11-20 14:48:57 +0100

    Add more consoles and rom extensions

--- Open With Retroarch.bat
+++ Open With Retroarch.bat
@@ -19,19 +19,23 @@ goto %rom-ext%
 :nintendo_consoles
 
     :.nes
+    :.fds
+    :.fam
         set "emu-system=nes"
         set "emu-core=mesen"
         goto run
 
     :.smc
     :.sfc
+    :.srm
         set "emu-system=snes"
         set "emu-core=snes9x"
         goto run
 
     :.n64
-    :.z64
     :.v64
+    :.z64
+    :.ndd
         set "emu-system=n64"
         set "emu-core=mupen64plus_next"
         @REM set "emu-core=parallel"
@@ -39,12 +43,15 @@ goto %rom-ext%
 
     :.gc
     :.gcz
+    :.gcm
         set "emu-system=gc"
         set "emu-core=dolphin"
         goto run
 
     :.wii
     :.wad
+    :.wbfs
+    :.wdf
         set "emu-system=wii"
         set "emu-core=dolphin"
         goto run
@@ -77,6 +84,7 @@ goto %rom-ext%
 
     :.3ds
     :.3dsx
+    :.3dz
     :.cia
         set "emu-system=3ds"
         set "emu-core=citra"
@@ -97,12 +105,12 @@ goto %rom-ext%
 
     :.psx
         set "emu-system=psx"
-        set "emu-core="
+        set "emu-core=pcsx_rearmed"
         goto run
 
     :.ps2
         set "emu-system=ps2"
-        set "emu-core="
+        set "emu-core=pcsx2"
         goto run
 
     :.ps3
@@ -112,6 +120,91 @@ goto %rom-ext%
 
     :.ps4
         set "emu-system=ps4"
+        set "emu-core="
+        goto run
+
+
+:sega_consoles
+    :.sms
+        set "emu-system=sega-master-system"
+        set "emu-core=genesis_plus_gx"
+        goto run
+    
+    :.smd
+    :.gen
+        set "emu-system=sega-genesis"
+        set "emu-core=genesis_plus_gx"
+        goto run
+
+    :.gg
+        set "emu-system=sega-game-gear"
+        set "emu-core=genesis_plus_gx"
+        goto run
+
+    :.32x
+        set "emu-system=sega-32x"
+        set "emu-core=genesis_plus_gx"
+        goto run
+
+    :gdi
+        set "emu-system=sega-dreamcast"
+        set "emu-core=genesis_plus_gx"
+        goto run
+
+    :.neogeo
+        set "emu-system=neogeo"
+        set "emu-core=neocd"
+        goto run
+
+
+:atari_consoles
+
+    :.a26
+        set "emu-system=atari-2600"
+        set "emu-core=stella"
+        goto run
+
+    :.a52
+        set "emu-system=atari-5200"
+        set "emu-core=atari800"
+        goto run
+
+    :.a78
+        set "emu-system=atari-7800"
+        set "emu-core=prosystem"
+        goto run
+        
+    :.lnx
+        set "emu-system=atari-lynx"
+        set "emu-core="
+        goto run
+
+    :.jag
+    :.jcg
+        set "emu-system=atari-jaguar"
+        set "emu-core="
+        goto run
+
+
+:commodore_consoles
+
+    :.hdf
+        set "emu-system=commodore-amiga"
+        set "emu-core=puae"
+        goto run
+
+    :.c64
+        set "emu-system=commodore-64"
+        set "emu-core="
+        goto run
+
+    :.c128
+        set "emu-system=commodore-128"
+        set "emu-core=vice_x128"
+        goto run
+
+    :.vic
+        set "emu-system=commodore-vic20"
         set "emu-core="
         goto run
 
